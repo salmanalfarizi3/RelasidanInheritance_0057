b36991035c2c848bba364aa519da01b087104e78
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-05-27 15:29:23 +0700

    frist

---
+++ Asosiasi/Asosiasi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+
+class pasien { // Asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+};
