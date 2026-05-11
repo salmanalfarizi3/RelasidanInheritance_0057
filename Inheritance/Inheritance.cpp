546b276eec1a73193d08e2c203cfbc111fc220dc
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-11 18:30:53 +0700

    class mahasiswa dengan akses public

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -3,3 +3,20 @@
 using namespace std;
 
 class orang {
+public:
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+};
+
