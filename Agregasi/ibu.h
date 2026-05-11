0ae91304905012ea1ece311224059557582b16e7
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-05-11 18:27:50 +0700

    Dengan akses public

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -3,3 +3,17 @@
 #include <vector>
 
 class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
